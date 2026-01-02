476e890b878a12e23c3f9a52b7aeb3a44fe219b3
Vidhu Popli <vidhu278@gmail.com>
2026-01-02 16:44:55 +0530

    update gitignore file

--- sanbox.c
+++ sanbox.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-    char name[50];
-
-    strncpy(name, "Vidhu", sizeof(name) - 1); // The third arg is, dont copy into the array memory more than space available
-
-    printf("%s\n", name);
-
-    return 0;
+  return 0;
 }
